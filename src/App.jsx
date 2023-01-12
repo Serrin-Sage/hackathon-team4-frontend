2c10189a73e17afe945626adbffc41761450c7f1
Amneet Singh <+>
2023-01-12 15:36:04 -0500

    tablepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
+import TablePage from './components/TablePage'
 function App() {
   return (
     <div className="App">
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 import './stylesheets/landingpage.css'
+import './stylesheets/tablepage.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
