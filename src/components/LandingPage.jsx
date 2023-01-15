47ae718d7d4153c33937afae90dfa2f2f0fb1710
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-01-14 21:13:50 -0500

    added basic signin functionality, removed manager and stff tabs

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,28 +1,30 @@
 import { useState } from "react"
-
+import { useNavigate } from "react-router-dom"
 const LandingPage = () => {
   const [admin, setAdmin] = useState(false)
+  const [input, setInput] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    fetch(`http://localhost:3000/staff/${input}`)
+      .then(res => res.json())
+      .then(data => {
+        if (data.is_manager === true) navigate('/manager')
+        else if (data.is_manager === false) navigate('/menu')
+      })
+      .catch(() => console.log("OOPS"))
+  }
+
   return (
     <div>
-      <div className="top-tabs">
-        <div className={admin ? "active" : "manager-tag"} onClick={() => setAdmin(true)}>
-          Manager
-        </div>
-        <div className={admin ? "staff-tab" : "active"} onClick={() => setAdmin(false)}>
-          Staff
-        </div>
-      </div>
       <div className="input-container">
-        {admin ?
+        <form onSubmit={handleSubmit}>
           <label className="input-label">
-            Enter Manager ID <br />
-            <input type="text" className="input-box" spellCheck={false} />
-          </label> :
-          <label className="input-label">
-            Enter Staff ID <br />
-            <input type="text" className="input-box" spellCheck={false} />
+            Enter Employee ID <br />
+            <input type="text" className="input-box" spellCheck={false} onChange={(e) => setInput(e.target.value)}/>
           </label>
-        }
+        </form>
       </div>
     </div>
   )
