98544d332e1901e3d62b9430ff261ee3603ea0ff
Amneet Singh <+>
2023-01-17 17:59:25 -0500

    fix tables-2

--- src/components/TablePage.jsx
+++ src/components/TablePage.jsx
@@ -1,86 +1,69 @@
 const TablePage = ({ employeeView }) => {
     return (
         <div className={employeeView}>
-            <div className="table-1" id="table-1">
-                <div className="chair-1" id="1">
-                </div>
-                <div className="chair-2" id="2">
-                </div>
-                <div className="chair-3" id="3">
-                </div>
-                <div className="chair-4" id="4">
-                </div>
+            <div className="table-1" id="table-1" >
+                <img src="src/assets/new-chair.png" className="chair-1" id="1">
+                </img>
+                <img src="src/assets/new-chair.png" className="chair-2" id="2">
+                </img>
+                <img src="src/assets/chair.png" className="chair-3" id="3">
+                </img>
+                <img src="src/assets/chair.png" className="chair-4" id="4">
+                </img>
             </div>
-
             <div className="table-2" id="table-2">
-                <div className="chair-1" id="5">
-                </div>
-                <div className="chair-2" id="6">
-                </div>
-                <div className="chair-3" id="7">
-                </div>
-                <div className="chair-4" id="8">
-                </div>
+                <img src="src/assets/new-chair.png" className="chair-1" id="5">
+                </img>
+                <img src="src/assets/new-chair.png" className="chair-2" id="6">
+                </img>
+                <img src="src/assets/chair.png" className="chair-3" id="7">
+                </img>
+                <img src="src/assets/chair.png" className="chair-4" id="8">
+                </img>
             </div>
-
             <div className="table-3" id="table-3">
-                <div className="chair-1a" id="9">
-                </div>
-                <div className="chair-2a" id="10">
-                </div>
+                <img src="src/assets/new-chair.png" className="chair-1a" id="9">
+                </img>
+                <img src="src/assets/chair.png" className="chair-2a" id="10">
+                </img>
             </div>
-
             <div className="table-4" id="table-4">
-                <div className="chair-1a" id="11">
-                </div>
-                <div className="chair-2a" id="12">
-                </div>
+                <img src="src/assets/new-chair.png" className="chair-1a" id="11">
+                </img>
+                <img src="src/assets/chair.png" className="chair-2a" id="12">
+                </img>
             </div>
-
             <div className="table-5" id="table-5">
-                <div className="chair-1b" id="13">
-                </div>
-                <div className="chair-2b" id="14">
-                </div>
-                <div className="chair-3b" id="15">
-                </div>
-                <div className="chair-4b" id="16">
-                </div>
+                <img src="src/assets/new-chair.png" className="chair-1b" id="13">
+                </img>
+                <img src="src/assets/new-chair.png" className="chair-2b" id="14">
+                </img>
+                <img src="src/assets/chair.png" className="chair-3b" id="15">
+                </img>
+                <img src="src/assets/chair.png" className="chair-4b" id="16">
+                </img>
             </div>
-
             <div className="table-6" id="table-6">
-                <div className="chair-1b" id="17">
-                </div>
-                <div className="chair-2b" id="18">
-                </div>
-                <div className="chair-3b" id="19">
-                </div>
-                <div className="chair-4b" id="20">
-                </div>
+                <img src="src/assets/new-chair.png" className="chair-1b" id="17">
+                </img>
+                <img src="src/assets/new-chair.png" className="chair-2b" id="18">
+                </img>
+                <img src="src/assets/chair.png" className="chair-3b" id="19">
+                </img>
+                <img src="src/assets/chair.png" className="chair-4b" id="20">
+                </img>
             </div>
 
             <div className="table-7" id="table-7">
-                <div className="chair-1b" id="21">
-                </div>
-                <div className="chair-2b" id="22">
-                </div>
-                <div className="chair-3b" id="23">
-                </div>
-                <div className="chair-4b" id="24">
-                </div>
+                <img src="src/assets/new-chair.png" className="chair-1c" id="21">
+                </img>
+                <img src="src/assets/new-chair.png" className="chair-2c" id="22">
+                </img>
+                <img src="src/assets/new-chair.png" className="chair-3c" id="23">
+                </img>
+                <img src="src/assets/new-chair.png" className="chair-4c" id="24">
+                </img>
             </div>
-
-            <div className="table-8" id="table-8">
-                <div className="chair-1c" id="25">
-                </div>
-                <div className="chair-2c" id="26">
-                </div>
-                <div className="chair-3c" id="27">
-                </div>
-                <div className="chair-4c" id="28">
-                </div>
-            </div>
-
         </div>
 
     )
