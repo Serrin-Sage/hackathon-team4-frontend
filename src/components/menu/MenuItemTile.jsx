d3ab4386197033c579e3f70faae36434c1e66adc
Zak <zakedoubled@gmail.com>
2023-01-18 13:31:44 -0500

    render menu by category

--- src/components/menu/MenuItemTile.jsx
+++ src/components/menu/MenuItemTile.jsx
@@ -1,8 +1,31 @@
-const MenuItemTile = () => {
+const MenuItemTile = ({item}) => {
+
+    const [instructions, setInstructions] = useState('')
+
+    function handleInstructions(e){
+        setInstructions(e.target.value)
+    }
+
+    function addItemToOrder(){
+        fetch('http://localhost:3000/receipt_items', {
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({'order_id':'', 'item_name':item.name, 'item_price': item.price, 'instructions': instructions}),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+            })
+    }
     return (
         <div>
-            <img></img>
-            <h5></h5>
+            <button>
+            <p>{item.name}</p>
+            <p>{item.price}</p>
+            </button>
+            <input type = 'text' value = {instructions} onChange = {handleInstructions}></input>
         </div>
     )
 }
