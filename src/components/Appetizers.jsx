0ff4a6101b0218eb7cfa34cb09690150eede6861
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-01-18 23:14:54 -0500

    added new menu page, with bev, appz, entree, desrt components

---
+++ src/components/Appetizers.jsx
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react"
+
+const Appetizers = ({ setSelectedItemList }) => {
+    const [appsList, setAppsList] = useState([])
+    useEffect(() => {
+        const fetchApps = async () => {
+            let req = await fetch("http://localhost:3000/menu_items/Appetizer")
+            let res = await req.json()
+            if (req.ok) {
+                setAppsList(res)
+            }
+        }
+        fetchApps()
+
+    }, [])
+
+    const addAppToOrder = (appetizer) => {
+        setSelectedItemList((prevState) => {
+            return [...prevState, appetizer]
+        })
+    }
+    
+    return (
+        <div>
+            {
+                appsList.map((appetizer) => {
+                    return (
+                        <div key={appetizer.id} onClick={() => addAppToOrder(appetizer)}>
+                            {appetizer.name}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Appetizers
