0fc08b2033c8a695199883e4cc9117a18730a6de
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-01-20 10:15:31 -0500

    removed menu folder and renamed NewMenuPage to MenuPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './components/LandingPage'
-import TablePage from './components/TablePage'
-import MenuPage from './components/menu/MenuPage'
 
-import NewMenuPage from './components/NewMenuPage'
-
+import MenuPage from './components/MenuPage'
 import ManagerPage from './components/ManagerPage'
-import MenuItemsContainer from './components/menu/MenuItemsContainer'
-import MenuCategoryNavBar from './components/menu/MenuCategoryNavBar'
 
 function App() {
   const [currentStaff, setCurrentStaff] = useState({})
@@ -17,7 +12,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage setCurrentStaff={setCurrentStaff}/>} />
-        <Route path="/menu" element={<NewMenuPage currentStaff={currentStaff}/>} />
+        <Route path="/menu" element={<MenuPage currentStaff={currentStaff}/>} />
         <Route path="/manager" element={<ManagerPage />} />
       </Routes>
     </div>
