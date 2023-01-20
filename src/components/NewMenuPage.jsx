c40f85d96830cad7456d5e9d99445444a62fbe3b
serrin_sage <serrin.doscher53@myhunter.cuny.edu>
2023-01-20 09:46:24 -0500

    removed Beverage, Appetizer, Entree and Dessert Component to consolidate into MenuCategory

--- src/components/NewMenuPage.jsx
+++ src/components/NewMenuPage.jsx
@@ -1,14 +1,9 @@
 import { useState, useEffect } from "react"
 
 import MenuHeader from "./MenuHeader"
-
 import MenuCategory from "./MenuCategory"
-
 import Receipt from "./Receipt"
-import Beverages from "./Beverages"
-import Appetizers from "./Appetizers"
-import Entrees from "./Entrees"
-import Desserts from "./Desserts"
+
 
 const NewMenuPage = ({ currentStaff }) => {
 
@@ -18,39 +13,11 @@ const NewMenuPage = ({ currentStaff }) => {
         })
     }, [])
 
-    // const [showBevs, setShowBevs] = useState(true)
-    // const [showApps, setShowApps] = useState(false)
-    // const [showEntrees, setShowEntrees] = useState(false)
-    // const [showDesserts, setShowDesserts] = useState(false)
-
     const [selectedItemList, setSelectedItemList] = useState([])
     const [total, setTotal] = useState(0)
     const [currentTable, setCurrentTable] = useState('')
     const [selectedCategory, setSelectedCategory] = useState('Beverage')
     
-    // const clickCategory = (selection) => {
-    //     if (selection === "Beverages") {
-    //         setShowBevs(true)
-    //         setShowApps(false)
-    //         setShowEntrees(false)
-    //         setShowDesserts(false)
-    //     } else if (selection === "Appetizers") {
-    //         setShowBevs(false)
-    //         setShowApps(true)
-    //         setShowEntrees(false)
-    //         setShowDesserts(false)
-    //     } else if (selection === "Entrees"){
-    //         setShowBevs(false)
-    //         setShowApps(false)
-    //         setShowEntrees(true)
-    //         setShowDesserts(false)
-    //     } else if (selection === "Desserts"){
-    //         setShowBevs(false)
-    //         setShowApps(false)
-    //         setShowEntrees(false)
-    //         setShowDesserts(true)
-    //     }
-    // }
 
     function addToTotal(val){
         setTotal(total + val)
@@ -97,11 +64,6 @@ const NewMenuPage = ({ currentStaff }) => {
                         <div onClick={() => setSelectedCategory("Dessert")} className="category-title" id={selectedCategory === "Dessert" ? 'dessert-tab' : 'inactive-tab'}>Desserts</div>
                     </div>
                     <div className="menu-content">
-
-                        {/* {showBevs ? <Beverages addToTotal = {addToTotal} setSelectedItemList={setSelectedItemList}/> : null}
-                        {showApps ? <Appetizers addToTotal = {addToTotal} setSelectedItemList={setSelectedItemList}/> : null}
-                        {showEntrees ? <Entrees setSelectedItemList={setSelectedItemList}/> : null}
-                        {showDesserts ? <Desserts setSelectedItemList={setSelectedItemList}/> : null} */}
 
                         <MenuCategory addToTotal={addToTotal} updateFoodList={updateFoodList} selectedCategory={selectedCategory}/>
 
